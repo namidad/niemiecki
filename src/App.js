c3f3bfc1f82dc6bc619fa30805c09aee04fd95ac
namidad <salamonoliwier@gmail.com>
2019-03-26 16:36:15 +0100

    rodzajniki

--- src/App.js
+++ src/App.js
@@ -1,25 +1,152 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import slowka from './slowka'
 
 class App extends Component {
+
+  state={
+    slowka,
+    index: 0,
+    div1: {},
+    div2: {},
+    div3: {},
+    correct: 0,
+    incorrect: 0,
+  }
+
+  handleClick = (e) => {
+    if(e===1){
+      const newSlowka=this.state.slowka;
+      if(this.state.slowka[this.state.index].rodzajnik==="der"){ 
+        newSlowka[this.state.index].amount--;
+        if(newSlowka[this.state.index].amount===0){
+          newSlowka.splice(this.state.index,1);
+        }
+        this.setState({
+          slowka: newSlowka,
+          correct: this.state.correct+1,
+          div1: {background: "green",pointerEvents: "none"},
+          div2: {pointerEvents: "none"},
+          div3: {pointerEvents: "none"},
+        })
+      } else if(this.state.slowka[this.state.index].rodzajnik==="die"){
+          newSlowka[this.state.index].amount++;
+            this.setState({
+              div1: {background: "red", pointerEvents: "none"},
+              div2: {background: "green", pointerEvents: "none"},
+              div3: {pointerEvents: "none"},
+              incorrect: this.state.incorrect+1,
+            })
+          } else {
+            newSlowka[this.state.index].amount++;
+            this.setState({
+              div1: {background: "red", pointerEvents: "none"},
+              div2: {pointerEvents: "none"},
+              div3: {background: "green", pointerEvents: "none"},
+              incorrect: this.state.incorrect+1,
+            })
+          }
+    } else if (e===2) {
+      const newSlowka=this.state.slowka;
+      if(this.state.slowka[this.state.index].rodzajnik==="die"){
+        newSlowka[this.state.index].amount--;
+        if(newSlowka[this.state.index].amount===0){
+          newSlowka.splice(this.state.index,1);
+        }
+        this.setState({
+          slowka: newSlowka,
+          correct: this.state.correct+1,
+          div2: {background: "green",pointerEvents: "none"},
+          div1: {pointerEvents: "none"},
+          div3: {pointerEvents: "none"},
+        })
+      } else if(this.state.slowka[this.state.index].rodzajnik==="der"){
+        newSlowka[this.state.index].amount++;
+        this.setState({
+          div1: {background: "green", pointerEvents: "none"},
+          div2: {background: "red", pointerEvents: "none"},
+          div3: {pointerEvents: "none"},
+          incorrect: this.state.incorrect+1,
+        })
+      } else {
+        newSlowka[this.state.index].amount++;
+        this.setState({
+          div1: {pointerEvents: "none"},
+          div3: {background: "green", pointerEvents: "none"},
+          div2: {background: "red", pointerEvents: "none"},
+          incorrect: this.state.incorrect+1,
+        })
+      }
+    } else {
+      const newSlowka=this.state.slowka;
+      if(this.state.slowka[this.state.index].rodzajnik==="das"){
+        newSlowka[this.state.index].amount--;
+        if(newSlowka[this.state.index].amount===0){
+          newSlowka.splice(this.state.index,1);
+        }
+        this.setState({
+          slowka: newSlowka,
+          correct: this.state.correct+1,
+          div3: {background: "green",pointerEvents: "none"},
+          div2: {pointerEvents: "none"},
+          div1: {pointerEvents: "none"},
+        })
+      } else if(this.state.slowka[this.state.index].rodzajnik==="der"){
+        newSlowka[this.state.index].amount++;
+        this.setState({
+          div1: {background: "green", pointerEvents: "none"},
+          div2: {pointerEvents: "none"},
+          div3: {background: "red", pointerEvents: "none"},
+          incorrect: this.state.incorrect+1,
+        })
+      } else {
+        newSlowka[this.state.index].amount++;
+        this.setState({
+          div1: {pointerEvents: "none"},
+          div2: {background: "green", pointerEvents: "none"},
+          div3: {background: "red", pointerEvents: "none"},
+          incorrect: this.state.incorrect+1,
+        })
+    }
+  }
+  }
+
+  handleChange = () => {
+    const ind = Math.floor(Math.random()*this.state.slowka.length);
+    this.setState({
+      index: ind,
+      div1: {background: "white", pointerEvents: "auto"},
+      div2: {background: "white", pointerEvents: "auto"},
+      div3: {background: "white", pointerEvents: "auto"},
+    })
+  }
+
+
   render() {
+    console.log(this.state.slowka);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={"container"}>
+        <div className={"testContainer"}>
+          <h1 className={"wordBox"}>
+            {this.state.slowka[this.state.index].translated}
+          </h1>
+          <div className={"answerBox"}>
+            <div style={this.state.div1} onClick={e => this.handleClick(1)}>der</div>
+            <div style={this.state.div2} onClick={e => this.handleClick(2)}>die</div>
+            <div style={this.state.div3} onClick={e => this.handleClick(3)}>das</div>
+          </div>
+          <div>
+            <div onClick={this.handleChange} className={"nextWord"}>
+            Nastepne słówko
+            </div>
+            <div>Zostało słówek: {this.state.slowka.length}</div>
+            <div>Poprawne odpowiedzi: {this.state.correct}</div>
+            <div>Bledne odpowiedzi: {this.state.incorrect}</div>
+
+          </div>
+          
+        </div>
+        
       </div>
     );
   }
